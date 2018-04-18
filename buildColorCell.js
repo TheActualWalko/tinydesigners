913f55cb5aced6d367db47c59f048b461897fb87
Sam Watkinson <sam.ws.watkinson@gmail.com>
2018-04-18 16:57:00 -0700

    reformat colors

--- buildColorCell.js
+++ buildColorCell.js
@@ -1,4 +1,4 @@
 const {cssColor} = require('color-functions');
 
 const rgbString = ({r,g,b}) => `rgb(${r},${g},${b})`;
-module.exports = (color) => `<a style="background-color:${rgbString(cssColor(color.hex))}"><span>${color.code}</span></a>`;
+module.exports = ({hex, name}, url) => `<a href='${url}' style="background-color:${rgbString(cssColor(hex))}"><span>${name}</span></a>`;
--- main.js
+++ main.js
@@ -2,8 +2,19 @@ const express = require('express');
 const getColors = require('./getColors');
 const buildColorCell = require('./buildColorCell');
 const buildPage = require('./buildPage');
+const buildOrderForm = require('./buildOrderForm');
 const path = require('path');
 const router = express.Router();
+
+const urlFragments = {
+  T_SHIRT: 't-shirt',
+  FAV_COLOR: 'my-favorite-color'
+};
+
+const PRICE = 50 * 100;
+
+const buildSingleProductUrl = (type, line, slug) => `/${urlFragments[type]}/${urlFragments[line]}/${slug}`;
+const buildProductImageUrl = (type, line, slug) => `/images/${urlFragments[type]}/${urlFragments[line]}/${slug}`;
 
 router.get('/style.css', (req, res) => res.sendFile(path.join(__dirname, 'style.css')));
 router.get('/logo.png', (req, res) => res.sendFile(path.join(__dirname, 'logo.png')));
@@ -11,7 +22,11 @@ router.get('/logo.png', (req, res) => res.sendFile(path.join(__dirname, 'logo.pn
 router.get('/', (req, res) => {
   res.send(buildPage(`
     <h3>My favorite color is...</h3>
-    <section class='colors-list'>${getColors().map(buildColorCell).join('')}</section>
+    <section class='colors-list'>${
+      getColors()
+        .map((color) => buildColorCell(color, buildSingleProductUrl('T_SHIRT', 'FAV_COLOR', color.slug)))
+        .join('')
+    }</section>
   `));
 });
 
